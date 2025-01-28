53612125edcb750fcbecec1212d427e0eedd4342
Thosha Moodley <30285943+thoshamoodley@users.noreply.github.com>
2025-01-28 17:45:45 +0100

    Added vision functionality which takes an image on the local drive and sends it to openai

--- OpenAIConnectServer.py
+++ OpenAIConnectServer.py
@@ -12,6 +12,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from openai import OpenAI
 import os
 import glob
+import base64
+import requests
 
 #create an OpenAI client
 OpenAIclient = OpenAI()
@@ -46,7 +48,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 #get GPT invitation to converse
 
                 completion = OpenAIclient.chat.completions.create(
-                    model="gpt-3.5-turbo",
+                    model="gpt-4-turbo",
                     messages=self.mymessages
                 )
 
@@ -64,7 +66,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 print(self.mymessages)
                 # contact GPT to get a new context related response to the user input
                 completion = OpenAIclient.chat.completions.create(
-                    model="gpt-3.5-turbo",
+                    model="gpt-4o",
                     messages=self.mymessages
                 )
                 gptresponsetext = completion.choices[0].message.content
@@ -72,8 +74,52 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                 self.mymessages = self.mymessages.append({"role": "assistant", "content": gptresponsetext})
                 self.wfile.write(bytes(gptresponsetext, "utf-8"))
 
+            # use image input and have a conversation about it, separate from the chatbot functionality
+            elif params['action'][0] == 'vision':
+                # get file location from environmental variables
+                filelocation = os.environ['FILEDIR']
+                print(filelocation)
+
+                list_of_files = glob.glob(filelocation + '/*.jpg')
+                latest_file = max(list_of_files, key=os.path.getctime)
+                print(latest_file)
+                # Path to your image
+                image_path = latest_file
+
+                # Getting the base64 string
+                base64_image = encode_image(image_path)
+                vision_prompt_text = "the image is from the camera of a humanoid robot. what would the robot say about the image to start a conversation? you don't have to identify or make assumptions about people in images."
+
+                response = OpenAIclient.chat.completions.create(
+                    model="gpt-4o",
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {"type": "text", "text": vision_prompt_text},
+                                {
+                                    "type": "image_url",
+                                    "image_url": {
+                                        "url": f"data:image/jpeg;base64,{base64_image}"
+                                    },
+                                },
+                            ],
+                        }
+                    ],
+                    max_tokens=300,
+                )
+
+                print(response.choices[0])
+                gptresponsetext = response.choices[0].message.content
+
+                self.mymessages = self.mymessages.append({"role": "assistant", "content": gptresponsetext})
+                self.wfile.write(bytes(gptresponsetext, "utf-8"))
         else:
             self.wfile.write(b'no data')
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8001):
     # starts the HTTP server
@@ -86,7 +132,7 @@ def speechtotext():
     # retrieves the audiofile written by Java client and sends to OpenAI to convert to text. Returns the text
 
     # get file location from environmental variables
-    filelocation = os.environ['AUDIODIR']
+    filelocation = os.environ['FILEDIR']
     print(filelocation)
 
     list_of_files = glob.glob(filelocation + '/*.wav')
